fc0e4216f270156b34cf20a10eccdf562fd5f446
Haolun <chshawkn@hotmail.com>
2017-04-19 00:01:05 +0800

    fix install_waitforit

--- docker/install_waitforit.sh
+++ docker/install_waitforit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ -z "${build_fileserver}" ]; then build_fileserver="https://github.com"; fi
-RUN curl -Ls ${build_fileserver}/maxcnunes/waitforit/releases/download/v1.4.0/waitforit-linux_amd64 >> /usr/bin/waitforit && \
+curl -Ls ${build_fileserver}/maxcnunes/waitforit/releases/download/v1.4.0/waitforit-linux_amd64 >> /usr/bin/waitforit && \
     chmod 755 /usr/bin/waitforit
